ede1fcfc674ec8d932fdb3d47da329ff79dc5765
Cairo Flores <florescairo2902@gmail.com>
2021-11-08 15:28:24 -0800

    first deploy

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
 const Index = () => (
-    <div>
-        <h1>Home Page</h1>
+    <div className="home">
+        <div className="banner">
+            <div className="pfp"></div>
+            <div className="name">Cairo Flores</div>
+            <div className="nav">
+                <div className="link projects-link">Projects</div>
+                <div className="link experience-link">Experience</div>
+                <div className="link blog-link">Blog</div>
+            </div>
+        </div>
     </div>
 )
 
