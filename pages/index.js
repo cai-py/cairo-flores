68f3f033648bb42884b7f63e93eb32e6d30e904d
Cairo Flores <florescairo2902@gmail.com>
2021-11-08 15:56:43 -0800

    add db and sample data

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,20 @@
+import Link from 'next/link'
+
 const Index = () => (
     <div className="home">
         <div className="banner">
             <div className="pfp"></div>
             <div className="name">Cairo Flores</div>
             <div className="nav">
-                <div className="link projects-link">Projects</div>
-                <div className="link experience-link">Experience</div>
-                <div className="link blog-link">Blog</div>
+                <Link href='/projects'>
+                    <div className="link projects-link">Projects</div>
+                </Link>
+                <Link href='/experience'>
+                    <div className="link experience-link">Experience</div>
+                </Link>
+                <Link href='/blog'>
+                    <div className="link blog-link">Blog</div>
+                </Link>
             </div>
         </div>
     </div>
