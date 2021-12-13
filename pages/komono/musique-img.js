fe5dc087fff6d09cd1dd894aaddb15be5fbd2bf0
Cairo Flores <florescairo2902@gmail.com>
2021-12-13 12:46:18 -0800

    test img hosting on google drive

--- pages/komono/musique-img.js
+++ pages/komono/musique-img.js
@@ -1,7 +1,11 @@
 import Image from 'next/image'
 
 function appImage() {
-    return <Image src="/musique-img.png" layout="fill"/>
+    return <img src="https://drive.google.com/uc?id=117x1JUEbgXFNTwkJZ37NlM2esNccRRqc"></img>
+    // https://drive.google.com/uc?id=117x1JUEbgXFNTwkJZ37NlM2esNccRRqc
+    // image id: 117x1JUEbgXFNTwkJZ37NlM2esNccRRqc
+    // https://drive.google.com/file/d/117x1JUEbgXFNTwkJZ37NlM2esNccRRqc/view?usp=sharing
+    // <Image src="/musique-img.png" layout="fill"/>
 }
 
 export default appImage
