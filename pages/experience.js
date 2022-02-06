de41f35223ec5307cf7945d6bf4efdba6ef8e107
Cairo Flores <florescairo2902@gmail.com>
2022-02-06 15:29:47 -0800

    bones of experience page

--- pages/experience.js
+++ pages/experience.js
@@ -5,6 +5,32 @@ const Experience = () => {
         <div className='experiencePage'>
             <Layout>
                 <h1 className='text-5xl mb-5'>Experience</h1>
+                <ul className='experienceList'>
+                    <li className='mt-5'>
+                        <h2 className='text-3xl'>LA-Tech</h2>
+                        <ul>
+                            
+                        </ul>
+                    </li>
+                    <li className='mt-5'>
+                        <h2 className='text-3xl'>Direct Action Everywhere</h2>
+                        <ul>
+                            
+                        </ul>
+                    </li>
+                    <li className='mt-5'>
+                        <h2 className='text-3xl'>General Assmbly</h2>
+                        <ul>
+                            
+                        </ul>
+                    </li>
+                    <li className='mt-5'>
+                        <h2 className='text-3xl'>Self-Taught</h2>
+                        <ul>
+                            
+                        </ul>
+                    </li>
+                </ul>
             </Layout>
         </div>
     )
