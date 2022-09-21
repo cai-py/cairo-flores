85d651081c9dc13e9986e13eded0ea7ddd7f2c94
Cairo Flores <florescairo2902@gmail.com>
2022-09-20 19:08:42 -0700

    testing out displaying data from mongo

---
+++ pages/test.js
@@ -0,0 +1,36 @@
+import { MONGO_CLIENT_EVENTS } from "mongodb"
+import client from "../lib/mongodb"
+
+export default function Projects({ projects }) {
+    return (
+        <div>
+            <ul>
+                {projects.map((project) => {
+                    <li>
+                        <h1>{project.projectName}</h1>
+                    </li>
+                })}
+            </ul>
+        </div>
+    )
+}
+
+export async function getServerSideProps() {
+    try {
+        const client = await client;
+        const db = client.db("Portfolio_Data");
+
+        const projects = await db
+            .collection('Project_Data')
+            .find({})
+            .sort({ metacritic: -1 })
+            .limit(20)
+            .toArray();
+
+        return {
+            props: { projects:JSON.parse(JSON.stringify(projects)) },
+        }
+    } catch (e) {
+        console.error(e)
+    }
+}
