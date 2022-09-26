091fbf1986693ebb1d5b9f76316cdef9eb72785b
Cairo Flores <florescairo2902@gmail.com>
2022-09-25 18:16:09 -0700

    copy ts connection file from mongodb example2

---
+++ lib/mongodb.ts
@@ -0,0 +1,74 @@
+import { MongoClient } from 'mongodb'
+
+if (!process.env.MONGODB_URI) {
+  throw new Error('Invalid environment variable: "MONGODB_URI"')
+}
+
+const uri = process.env.MONGODB_URI
+const options = {}
+
+let client
+let clientPromise: Promise<MongoClient>
+
+if (!process.env.MONGODB_URI) {
+  throw new Error('Please add your Mongo URI to .env.local')
+}
+
+if (process.env.NODE_ENV === 'development') {
+  // In development mode, use a global variable so that the value
+  // is preserved across module reloads caused by HMR (Hot Module Replacement).
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri, options)
+    global._mongoClientPromise = client.connect()
+  }
+  clientPromise = global._mongoClientPromise
+} else {
+  // In production mode, it's best to not use a global variable.
+  client = new MongoClient(uri, options)
+  clientPromise = client.connect()
+}
+
+// Export a module-scoped MongoClient promise. By doing this in a
+// separate module, the client can be shared across functions.
+export default clientPromise
+
+
+
+
+
+// const {MongoClient} = require('mongodb')
+// require('dotenv').config()
+
+
+// async function main(){
+//     /**
+//      * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+//      * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+//      */
+//     // const uri = "mongodb+srv://Cai_py:9cKpksM3Cc39qp5@cluster0.jgcig.mongodb.net/test";
+//     const uri = process.env.MONGODB_URI
+
+//     const client = new MongoClient(uri);
+ 
+//     try {
+//         // Connect to the MongoDB cluster
+//         await client.connect();
+ 
+//         // Make the appropriate DB calls
+//         await  listDatabases(client);
+ 
+//     } catch (e) {
+//         console.error(e);
+//     } finally {
+//         await client.close();
+//     }
+// }
+
+// main().catch(console.error);
+
+// async function listDatabases(client){
+//     databasesList = await client.db().admin().listDatabases();
+ 
+//     console.log("Databases:");
+//     databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+// };
