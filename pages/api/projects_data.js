103a6da99ab52b32101025a9796a6bbabde7b6a3
Cairo Flores <florescairo2902@gmail.com>
2022-09-26 15:31:11 -0700

    api won't display data

--- pages/api/projects_data.js
+++ pages/api/projects_data.js
@@ -3,11 +3,12 @@ import clientPromise from "../../lib/mongodb.js";
 export default async (req, res) => {
    try {
        const client = await clientPromise;
-       const db = client.db("Projects");
+       const db = client.db("Portfolio_Data");
 
        const projects = await db
-           .collection("projects")
+           .collection("Projects")
            .find({})
+           .limit(10)
            .toArray();
 
        res.json(projects);
@@ -15,3 +16,24 @@ export default async (req, res) => {
        console.error(e);
    }
 };
+
+// NOTES
+
+// >>>>>>>>RETURNS THIS ERROR WHEN RUN >>>>>
+
+// cai.py@MacBook-Pro-2 cairo-flores % node pages/api/projects_data.js
+// (node:16363) Warning: To load an ES module, set "type": "module" in the package.json or use the .mjs extension.
+// (Use `node --trace-warnings ...` to show where the warning was created)
+// /Users/cai.py/Documents/career/cairo-flores/pages/api/projects_data.js:1
+// import clientPromise from "../../lib/mongodb.js";
+// ^^^^^^
+
+// SyntaxError: Cannot use import statement outside a module
+//     at Object.compileFunction (node:vm:352:18)
+//     at wrapSafe (node:internal/modules/cjs/loader:1031:15)
+//     at Module._compile (node:internal/modules/cjs/loader:1065:27)
+//     at Object.Module._extensions..js (node:internal/modules/cjs/loader:1153:10)
+//     at Module.load (node:internal/modules/cjs/loader:981:32)
+//     at Function.Module._load (node:internal/modules/cjs/loader:822:12)
+//     at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:81:12)
+//     at node:internal/main/run_main_module:17:47
