332b4fa7c69a66c7c194cdfd54775e1078336cc5
Cairo Flores <florescairo2902@gmail.com>
2022-10-11 14:50:14 -0700

    read route for projects

--- pages/api/projectsAPI/read.js
+++ pages/api/projectsAPI/read.js
@@ -1,13 +1,12 @@
 import connectMongo from "../../../lib/mongodb";
 import Project from "../../../lib/models/projectModel";
 
-export default async function addProject(req,res) {
+export default async function read(req,res) {
     try {
         await connectMongo()
-        project = await Project.create(req.body);
         const data = await Project.find()
-        res.json({ data })
-    } catch (error) {
+        res.json({data})
+    }catch (error) {
         console.log(error)
         res.json({error})
     }
